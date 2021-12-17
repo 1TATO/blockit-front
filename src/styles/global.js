e794cabfbeaef61159eb709d69bbbd543bec89d5
1TATO <josenetopr5@gmail.com>
2021-12-17 15:40:54 -0300

    First commit

---
+++ src/styles/global.js
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #1B2838;
+    --header: #1F3050;
+    --gray: #C5C3C0;
+    --white: #fff;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    color: var(--white);
+    background: var(--background);
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+ 
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
