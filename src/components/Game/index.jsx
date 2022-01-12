9287d746cdf48f2db96ec82630c376f8afbd37b8
1TATO <josenetopr5@gmail.com>
2022-01-12 13:20:42 -0300

    API connection

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,53 +1,38 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FaHeart, FaCartPlus } from "react-icons/fa";
+
+import api from "../../services/api";
 
 import gameImg from "../../assets/cs.jpg";
 
 import { Container, Content, GameContent, Details, Icons } from "./styles";
 
 export function Game() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    api.get("/products").then((response) => setProducts(response.data));
+  }, []);
+
   return (
     <Container>
       <Content>
-        <Link to={"/details"}>
-          <GameContent>
-            <img src={gameImg} alt="Game background" />
-            <Details>
-              <span>Counter Strike: Global Offensive</span>
-              <span>R$ 18,90</span>
-            </Details>
-            <Icons>
-              <FaHeart />
-              <FaCartPlus />
-            </Icons>
-          </GameContent>
-        </Link>
-        <Link to={"/details"}>
-          <GameContent>
-            <img src={gameImg} alt="Game background" />
-            <Details>
-              <span>Counter Strike: Global Offensive</span>
-              <span>R$ 18,90</span>
-            </Details>
-            <Icons>
-              <FaHeart />
-              <FaCartPlus />
-            </Icons>
-          </GameContent>
-        </Link>
-        <Link to={"/details"}>
-          <GameContent>
-            <img src={gameImg} alt="Game background" />
-            <Details>
-              <span>Counter Strike: Global Offensive</span>
-              <span>R$ 18,90</span>
-            </Details>
-            <Icons>
-              <FaHeart />
-              <FaCartPlus />
-            </Icons>
-          </GameContent>
-        </Link>
+        {products.map((product) => (
+          <Link key={product.id} to={`/details/${product.id}`}>
+            <GameContent>
+              <img src={product.imageUrl} alt={product.name} />
+              <Details>
+                <span>{product.name}</span>
+                <span>R$ {product.price}</span>
+              </Details>
+              <Icons>
+                <FaHeart />
+                <FaCartPlus />
+              </Icons>
+            </GameContent>
+          </Link>
+        ))}
       </Content>
     </Container>
   );
